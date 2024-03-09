b5242c01ad8653d835cd85585eb2fa0722c88d49
Scarlett <Scarlett@kayde.me>
2024-03-09 15:53:11 +0000

    cleaning up the main page some more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
   return (
     <>
-    <div className="md:hidden">
+    {/* <div className="md:hidden">
         <img
             src="/examples/authentication-light.png"
             width={1280}
@@ -29,17 +29,8 @@ export default async function Home() {
             alt="Authentication"
             className="hidden dark:block"
         />
-    </div>
+    </div> */}
       <div className="container relative hidden h-dvh flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -58,10 +49,10 @@ export default async function Home() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Sign in to K-Note
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Click the button below to sign in with Discord
               </p>
             </div>
             <UserAuthForm />
