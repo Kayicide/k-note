691ab32d8596bfe0e8578865be609f68f61625f2
Scarlett <Scarlett@kayde.me>
2024-03-09 22:07:56 +0000

    cleanup + db changes

---
+++ prisma/migrations/20240309215241_adding_notes/migration.sql
@@ -0,0 +1,40 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_createdById_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "NoteBook" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "NoteBook_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "notebookId" TEXT NOT NULL,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "NoteBook" ADD CONSTRAINT "NoteBook_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_notebookId_fkey" FOREIGN KEY ("notebookId") REFERENCES "NoteBook"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,59 +2,64 @@ import Link from "next/link";
 import { unstable_noStore as noStore } from "next/cache";
 import { UserAuthForm } from "~/app/_components/user-auth-form";
 import { getServerAuthSession } from "~/server/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  noStore();
-  const session = await getServerAuthSession();
+    noStore();
+    const session = await getServerAuthSession();
 
-  return (
-    <>
-      <div className="container relative h-dvh flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            K-Note
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;WOW THIS IS SUCH A COOL SITE&rdquo;
-              </p>
-              <footer className="text-sm">Kay</footer>
-            </blockquote>
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Sign in to K-Note
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Click the button below to sign in with Discord
-              </p>
+    if(session){
+        redirect("/notes");
+    }
+
+    return (
+        <>
+            <div className="container relative h-dvh flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+                <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+                    <div className="absolute inset-0 bg-zinc-900" />
+                        <div className="relative z-20 flex items-center text-lg font-medium">
+                        K-Note
+                        </div>
+                        <div className="relative z-20 mt-auto">
+                            <blockquote className="space-y-2">
+                                <p className="text-lg">
+                                    &ldquo;WOW THIS IS SUCH A COOL SITE&rdquo;
+                                </p>
+                                <footer className="text-sm">Kay</footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div className="lg:p-8">
+                        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                            <div className="flex flex-col space-y-2 text-center">
+                                <h1 className="text-2xl font-semibold tracking-tight">
+                                    Sign in to K-Note
+                                </h1>
+                                <p className="text-sm text-muted-foreground">
+                                    Click the button below to sign in with Discord
+                                </p>
+                            </div>
+                            <UserAuthForm />
+                            <p className="px-8 text-center text-sm text-muted-foreground">
+                            By clicking continue, you agree to our{" "}
+                            <Link
+                                href="/terms"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                Terms of Service
+                            </Link>{" "}
+                            and{" "}
+                            <Link
+                                href="/privacy"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                Privacy Policy
+                            </Link>
+                            .
+                            </p>
+                    </div>
+                </div>
             </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
-          </div>
-        </div>
-      </div>
-      </>
-  );
+        </>
+    );
 }
