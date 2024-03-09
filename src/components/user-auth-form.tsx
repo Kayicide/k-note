fce77304d8a7710920ac81cd5509d40782930c0a
Scarlett <Scarlett@kayde.me>
2024-03-09 16:11:18 +0000

    fixing display on small screens

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -12,25 +12,10 @@ import { Label } from "~/components/ui/label"
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
-
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
-
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
-  }
-
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <Button variant="outline" type="button" disabled={isLoading} onClick={() => signIn("discord")}>
-        {isLoading ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.discord className="mr-2 h-4 w-4" />
-        )}{" "}
+      <Button variant="outline" type="button" onClick={() => signIn("discord")}>
+        <Icons.discord className="mr-2 h-4 w-4" />
         Discord
       </Button>
     </div>
