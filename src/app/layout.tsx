0e4dc4f8a38494e6828a49de109b0aae9cb42f5f
Scarlett <Scarlett@kayde.me>
2024-03-09 15:23:13 +0000

    initial homepage work, needs a lot more work but this is a start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { ThemeProvider } from "~/components/theme-provider";
+import { cn } from "~/lib/utils";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,9 +23,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body  className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            `font-sans ${inter.variable}`
+          )}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TRPCReactProvider>
+            <div className="relative flex min-h-screen flex-col bg-background">
+              {/* header */}
+              <main className="flex-1">
+                {children}
+              </main>
+              {/* footer */}
+            </div>
+          </TRPCReactProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
